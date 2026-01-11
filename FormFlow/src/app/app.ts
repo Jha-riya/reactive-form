b867b8b76d7563a33bf81507d4e779002e3007a7
scarlet_witch <riya.jha@ilmtec.in>
2026-01-11 23:19:25 +0530

    added user service

--- FormFlow/src/app/app.ts
+++ FormFlow/src/app/app.ts
@@ -1,12 +1,15 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, signal} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+    selector: 'app-root',
+    imports: [RouterOutlet],
+    templateUrl: './app.html',
+    standalone: true,
+    styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('FormFlow');
+    protected readonly title = signal('FormFlow');
+
+
 }
